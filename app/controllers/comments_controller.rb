28dac3f17170a299d6c26f6c7429438e5ffb5a1b
marcioaleson <marcioaleson@gmail.com>
2018-02-05 22:56:48 -0300

    Destroy comment added.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,12 @@ class CommentsController < ApplicationController
 		else
 			render 'new'
 		end
-
 	end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
 end
